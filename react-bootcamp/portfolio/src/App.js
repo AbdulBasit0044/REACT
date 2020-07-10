a59174efd03c80a4f8452864e5bbe59aa4882612
AbdulBasit0044 <abdulbasit0044@gmail.com>
2020-07-11 01:11:40 +0500

    ok

---
+++ react-bootcamp/portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class App extends Component{
+
+    render(){
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>This is something i always wanted to do.</p>
+            </div>
+        )
+    }
+}
+export default App;
